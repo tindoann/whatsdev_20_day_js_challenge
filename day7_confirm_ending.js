a75c310230bf7c8665b49edb452ace0f30078cc3
TinX <TinX@TinX.fios-router.home>
2019-03-06 00:51:03 -0500

    update

---
+++ day7_confirm_ending.js
@@ -0,0 +1,29 @@
+//check if a string(first argument, str) ends with the given target string(second argument, target). 
+function confirmEnding (str, target) {
+	'Never give up and good luck will find you.'
+--Flcor
+  if (str.substr(-target.length) === target) {
+	return true;
+} else {
+	return false	
+  }
+}
+confirmEnding('Bastian', 'noo'); 
+// condense version //
+
+
+function confirmEnding(str, target){
+  return str.substr(-target.length) === target; 
+}
+
+
+// other version //
+
+function confirmEnding(str, target){
+  return str.slice(-target.length) === target; 
+}
+
+confirmEnding("bastian", "noo"); 
+
+
+
